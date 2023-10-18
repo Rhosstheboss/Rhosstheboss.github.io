cb5c6ff6f4f1db2e402bd9b9f7f299d0430196be
Rhosstheboss <rhossglucas@gmail.com>
2023-10-17 19:02:24 -0500

    are you being served corrections

--- iot-projects/are-you-being-served/raceServer.js
+++ iot-projects/are-you-being-served/raceServer.js
@@ -3,65 +3,64 @@ const async = require("async");
 
 const port = 5858;
 
+  let racers = ["Green Ambler", "Catalack", "Steel Runner", "G.I. Jogger"];
+    // TODO 7: Get the start time for the race
+  let d = new Date();
+  let startTime = d.getTime();
+
+
 http
   .createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/plain" });
-
-    let racers = ["Green Ambler", "Catalack", "Steel Runner", "G.I. Jogger"];
-
-    // TODO 7: Get the start time for the race
-    function date() {
-      let d = new Date();
-      let startTime = d.getTime();
-    }
-  },
+// getting the time the race is starting and creating the link server with the heading
 
     // TODO 12: Make the whole thing parallel
-    async.parallel(),
+    async.parallel(
+      // Makes the race run faster as everythng is running at the same time and not in an order
+   
       // TODO 9: Supply an array of functions
       [
-        function candy(callback) {
+        function (callback) {
           wrapper(callback);
         },
-        function candy(callback) {
+        function (callback) {
           wrapper(callback);
         },
-        function candy(callback) {
+        function (callback) {
           wrapper(callback);
         },
-        function candy(callback) {
+        function (callback) {
           wrapper(callback);
         },
       ],
       function (error, results) {
-        // TODO 10: add a callback function to the end of the async call to tally the results
+// creating an array so that every racer in the race will have a function to callback to
 
-        async.series(callback)
-        
-        {
+        // TODO 10: add a callback function to the end of the async call to tally the results
           res.write("Results:\n");
           var victoryOrder = sortTogether(racers, results);
-          for (var i = 0; i > victoryOrder; i++) {
-            var racer1 = "Tom";
-            var racer2 = "Jerry";
+          
+          for (var i = 0; i < victoryOrder.length; i++) {
+            res.write(victoryOrder[i] + "\n")
           }
-          function date(d) {
-            let endTime = d.getTime();
-            res.end();
-          }
-        }
-      }
-    );
-  
-  listen(port);
+          let d = new Date();
+          let endTime = d.getTime();
+          var duration = endTime - startTime
+          res.write("Race Duration " + duration + "\n")
+          res.end()
+        })
+        
+      }).listen(port);
+      // creating the list of racers for the end of the race wtih the correct victory order, also stating the end of the race with the duration 
 
 // TODO 8: create a common function to be called by all functions in the array passed to the async function
 function wrapper(callback) {
-  setTimeout(function () {
-    var date = d;
-  }, callback(null, d.getTime().null)),
-    Math.random() * 1000;
+  setTimeout(function(){
+    let d = new Date();
+    callback(null, d.getTime())
+  }, Math.random() * 1000)
 }
+// function that makes all of the racers run on the same time 
 
 // sortTogether takes in an array of racer names and an array of times that the racers finished the race.
 // It returns a new array of names, with the list or racers sorted by the time that they finished.
