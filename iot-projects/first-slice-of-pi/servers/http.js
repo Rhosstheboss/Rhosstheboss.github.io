852a19d4878391dcef6a63bb7318218eba4fdee9
Rhosstheboss <rhossglucas@gmail.com>
2023-12-19 17:22:04 -0600

    pushing to pi

--- iot-projects/first-slice-of-pi/servers/http.js
+++ iot-projects/first-slice-of-pi/servers/http.js
@@ -1,5 +1,9 @@
 var sensorRoutes = require('./../routes/sensors');
 var actuatorRoutes = require('./../routes/actuators');
+var middleWare =  require('./../middleware/converter')
+var bodyParser = require('body-parser')
+
+app.use(bodyParser.json());
 
 const express = require('express'),
 	cors = require('cors');
@@ -15,6 +19,8 @@ const express = require('express'),
 	app.get('/pi', function(rep, res){
 		res.send('access the root response of my pi');
 	});
+
+	app.use(converter());
 
 module.exports = app;
 // I have looked through all files
